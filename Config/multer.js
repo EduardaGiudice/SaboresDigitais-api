52d58f56a42f309b38d2b0bdc684c91cddc02043
Eduarda Giudice <100857581+EduardaGiudice@users.noreply.github.com>
2024-04-11 22:53:44 -0300

    Update multer.js

--- Config/multer.js
+++ Config/multer.js
@@ -3,13 +3,9 @@ const multer = require('multer')
 //definindo storage
 
 const storage = multer.diskStorage({
-  destination: function(req, file, callback){
-    callback(null, './uploads')
-  },
   filename: function(req,file,callback) {
     callback(null,Date.now()+ file.originalname)
   }
-
 })
 
 //upload parametros
